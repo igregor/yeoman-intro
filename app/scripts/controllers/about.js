3f1202b5f6ea03b742c913a33c5a7c6cac02f222
igregorgregbook <grzegorz_wojtanowicz@wp.pl>
2016-03-06 19:50:37 +0100

    introducion from yeoman compleated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanIntroApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yeomanIntroApp
+ */
+angular.module('yeomanIntroApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
